fb5d9cf7e9c8bdca86ec9518f0a37da9d83573c4
Joff Harris <jonathan@harrisnet.co.uk>
2018-12-04 22:19:07 +0000

    Adding main

--- scrabble_points_calculator.py
+++ scrabble_points_calculator.py
@@ -1,3 +1,5 @@
+from sys import argv
+
 letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
 
@@ -6,22 +8,43 @@ letter_to_points[" "] = 0
 
 def score_word(word):
   point_total = 0
+
   for letter in word:
     point_total += letter_to_points.get(letter.upper(), 0)
+
   return point_total
 
 player_to_words = {}
 player_to_points = {}
-  
+
 def update_points_totals():
+  player_points = 0
+
   for player in player_to_words:
-    player_points = 0
     words = player_to_words[player]
+
     for word in words:
       player_points += score_word(word)
+
     player_to_points[player] = player_points
-  print(player_to_points)
+
+  return(player_to_points)
+
 
 def play_word(player, word):
-  player_to_words[player].append(word)
+  try:
+     player_to_words[player].append(word)
+
+  except KeyError:
+    player_to_words[player] = word
+
   update_points_totals()
+
+def main():
+  play_word(argv[1], argv[2])
+
+  print(player_to_points)
+
+if __name__ == '__main__':
+  main()
+    
